900229156dddbaef7dbb828840ee11465af0ec4e
ortalyq <130251908+ortalyq@users.noreply.github.com>
2023-04-09 22:41:33 +0600

    Update index.js

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send(`
     <h1>Форма</h1>
-    <form action="/data" method="post">
-      <input type="text" name="username" placeholder="Имя пользователя">
-      <input type="email" name="email" placeholder="Email">
-      <input type="submit" value="Отправить">
-    </form>
+    <form>
+  <!-- Поле названия -->
+  <label for="name">Название:</label>
+  <input type="text" name="title" required>
+
+  <!-- Поле текста адаптивного размера -->
+  <label for="text">Не туралы:</label>
+  <textarea  name="content" rows="4" cols="50" required></textarea>
+
+  <!-- Список -->
+  <label for="list">Список:</label>
+  <select name="labels">
+    <option name="Ұсыныс">Ұсыныс</option>
+    <option name="Жаңалық">Жаңалық</option>
+    <option name="Тапсырма">Тапсырма</option>
+  </select>
+
+  <!-- Кнопка отправки формы -->
+  <input type="submit" value="Отправить">
+</form>
+
   `);
 });
 
